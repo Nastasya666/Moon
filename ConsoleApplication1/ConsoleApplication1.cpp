a3fab212138c45a3f7419e81e616a836782cd5f9
Nastasya666 <vd50_n.i.fomenko@mpt.ru>
2019-11-28 11:14:22 +0300

    lol

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace  std;
+
+int main() 
+{
+	int c, d, a;
+	setlocale(0, "");
+	cin >> c;
+	cin >> d;
+	cin >> a;
+	cout << (atan(2 * c) / d + 2) / ((d - a) * (a - 1)); 
+	 
+}
